277026be344f73caa1a0c74c853a4e9fb1637309
Asad Ali <asad@freightexchange.com.au>
2022-02-20 12:30:52 +0530

    folder arrangements

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+       
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello Asad</h1>
+      </div>
+    )
+  }
+}
+export default App;
